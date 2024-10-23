d9588720dd3d6cb354e63eb2020d610cf2cf17c8
Ulaş Deniz Çakmazel <37212344+UlasDenizCakmazel@users.noreply.github.com>
2024-10-24 00:17:27 +0300

    Agent Walk
    ->Walk animation character
    ->Move background

--- Contra-Animation/Main.cpp
+++ Contra-Animation/Main.cpp
@@ -8,6 +8,15 @@ ICBYTES Corridor, Agent;
 ICBYTES AgentStanding;//Ajan ayakta dururken 
 //AJAN KOÞMA KARELERÝ ALTTAKÝ RESÝM DÝZÝNÝNE YÜKLENÝYOR
 ICBYTES AgentRun[8]; // <--Agent Running sequence will be uploaded here
+<<<<<<< Updated upstream
+=======
+
+
+int x = 120, y = 10;
+
+int Agent_x = 10;
+
+>>>>>>> Stashed changes
 void ICGUI_Create()
 {
 	ICG_MWSize(768, 672);
@@ -16,18 +25,92 @@ void ICGUI_Create()
 
 void LoadAgentRun()
 {
+<<<<<<< Updated upstream
 	Copy(Agent, 1, 1, 26, 44, AgentStanding);
 	PasteNon0(AgentStanding, 10, 58, Corridor);
 	ICBYTES cordinat{ {45, 9, 13,30},{71,9,16,30},{100,8,20,30},{130,8,23,30},{160,7,25,30},{189,8,22,30},
 		{218,8,25,31} };
+=======
+	
+	
+	//Copy(Map, x1, 100, x2, 370, Corridor);
+	
+	//Copy(Agent, 70, 9, 84, 34, AgentStanding);
+
+	
+	Copy(AgentX3, 1, 127, 55, 109, AgentStanding); //Koþan 1
+	//Copy(AgentX3, 53, 127, 62, 109, AgentStanding); //Koþan 2
+	//Copy(AgentX3, 112, 127, 67, 109, AgentStanding); //Koþan 3
+
+
+
+	PasteNon0(AgentStanding, 10, 250, Corridor);
+	DisplayImage(F1, Corridor);
+
+	Sleep(1000);
+	
+	
+	//MagnifyX3(AgentStanding, TV);
+	//SetPixelsX2(Agent, 0, 0, 0xcccccc, AgentStanding);
+	//Impress12x20(AgentStanding, 0, 0, "x3", 0x000000);
+	
+	
+	
+	
+	
+	//Paste(TV, 10, 200, Corridor);
+	
+	//ICBYTES cordinat{ {45, 9, 13,30},{71,9,16,30},{100,8,20,30},{130,8,23,30},{160,7,25,30},{189,8,22,30},
+		//{218,8,25,31} };
+>>>>>>> Stashed changes
 	//for (int i = 1; i <= cordinat.Y(); i++)
 	//{
 	//	Copy(Agent, cordinat.I(1,i), cordinat.I(2, i), cordinat.I(3, i), cordinat.I(4, i), AgentRun[i-1]);
 	//	PasteNon0(AgentRun[i-1], 33*i, 58, Corridor);
 	//}
+<<<<<<< Updated upstream
 	ICBYTES TV;
 	MagnifyX3(Corridor, TV);
 	DisplayImage(F1, TV);
+=======
+	
+	
+	//DisplayImage(F1, AgentStanding);
+	
+	
+
+	
+	
+	
+	int c = 0;
+	for (int i = 0; i < 50; i++) {
+		x += 40;
+		Copy(Map, x, y, 800, 450, Corridor);
+		if (c%2==0) {
+			Copy(AgentX3, 53, 127, 62, 109, AgentStanding); //Koþan 2
+			
+		}
+		else {
+			Copy(AgentX3, 112, 127, 67, 109, AgentStanding); //Koþan 3
+		}
+		c++;
+		Agent_x += 3;
+		//Paste(AgentStanding, Agent_x, 250, Corridor);
+		PasteNon0(AgentStanding, Agent_x, 250, Corridor);
+		DisplayImage(F1, Corridor);
+		Sleep(30);
+	}
+	
+	
+	
+
+	DisplayImage(F1, Corridor);
+
+	
+	//DisplayImage(F1, TV);
+	
+	
+>>>>>>> Stashed changes
 
 }
 
