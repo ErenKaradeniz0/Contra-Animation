7ed927f2e4ba223cf3b4ac11506d86f3644108db
ErenKaradeniz0 <erenkaradeniz011@gmail.com>
2024-10-25 17:02:36 +0300

    LoadAgentRun improved ScreenControllerThread added PrintAgent added

--- Contra-Animation/Main.cpp
+++ Contra-Animation/Main.cpp
@@ -3,12 +3,17 @@
 
 bool animation_paused = true;  // Animasyonu duraklatmak için yeni flag
 HANDLE hThread = NULL;  // Thread handle
+HANDLE th = NULL;
+
 
 int F1, F2;
 ICBYTES Map, Corridor, Agent, AgentX3;
-ICBYTES AgentStanding;
+ICBYTES AgentCurrent;
 int x = 1, y = 10;
 int Agent_x = 10;
+int Agent_y = 250;
+int Agent_state = 0;
+int Agent_phase = 0;
 
 void _WaitThread(HANDLE thread);
 void _CreateThread(HANDLE thread, void* threadMain);
@@ -18,83 +23,107 @@ void ICGUI_Create() {
     ICG_MWTitle("Contra");
 }
 
+// 0 hide
+// 1 stand
+// 2 run
+//currentScene
+
+void PrintAgent(int x, int y, int state, int Agent_phase)
+{
+    if (state == 0) //hide
+        return;
+
+    else if (state == 1) //durma
+    {
+        Copy(AgentX3, 1, 127, 55, 109, AgentCurrent);
+    }
+    else if (state == 2) //animasyon
+    {
+        if (Agent_phase % 5 == 0) Copy(AgentX3, 53, 127, 62, 109, AgentCurrent);
+        if (Agent_phase % 5 == 1) Copy(AgentX3, 112, 127, 67, 109, AgentCurrent);
+        if (Agent_phase % 5 == 2) Copy(AgentX3, 175, 127, 54, 109, AgentCurrent);
+        if (Agent_phase % 5 == 3) Copy(AgentX3, 230, 127, 56, 109, AgentCurrent);
+        if (Agent_phase % 5 == 4) Copy(AgentX3, 283, 127, 68, 109, AgentCurrent);
+
+    }
+    else if (state == 3) {
+        
+        if (Agent_phase % 4 == 0) {
+            Agent_x += 60;
+            Agent_y -= 50;
+            Copy(AgentX3, 345, 150, 60, 75, AgentCurrent);     //Flip 1
+        }
+        if (Agent_phase % 4 == 1) {
+            Agent_x += 60;
+            Agent_y -= 50;
+            Copy(AgentX3, 405, 150, 60, 75, AgentCurrent);     //Flip 2
+
+        }
+        if (Agent_phase % 4 == 2)
+        {
+            Agent_x += 60;
+            Agent_y += 50;
+            Copy(AgentX3, 465, 150, 60, 75, AgentCurrent);     //Flip 3
+        }   
+        if (Agent_phase % 4 == 3) {
+            Agent_x += 60;
+            Copy(AgentX3, 525, 150, 60, 75, AgentCurrent);     //Flip 4
+            Agent_state = 1;
+        }
+        Agent_phase++;
+
+
+        Sleep(100);
+    }
+    PasteNon0(AgentCurrent, x, y, Corridor); // Screen
+}
+
+
+void* ScreenControllerThread(LPVOID lpParam)
+{
+    while (true)
+    {
+        Copy(Map, x, y, 800, 450, Corridor);
+        // Arkaplan çizilecek
+
+        //Mapteki Objeler Çizilecek
+
+        //Karakterler Çizilecek
+        PrintAgent(Agent_x, Agent_y, Agent_state, Agent_phase);
+        // Projectilelar Çizilecek
+
+        //Sahneyi Goruntule
+        DisplayImage(F1, Corridor);
+    }
+}
+
 // Animasyon fonksiyonu
 void* LoadAgentRun(LPVOID lpParam) {
+    Agent_state = 1;
+    Sleep(1000);
     while (!animation_paused) { // Animasyon duraklatýlmamýþsa devam et
         int c = 1;
         Copy(Map, x, y, 800, 450, Corridor);
-        // Copy(AgentX3, 1, 127, 55, 109, AgentStanding); //Duran Adam
-        Copy(AgentX3, 53, 127, 62, 109, AgentStanding);  // Koþan Adam 1
-        PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-        DisplayImage(F1, Corridor);
-        Sleep(100);
+        // Copy(AgentX3, 1, 127, 55, 109, AgentCurrent); //Duran Adam
 
-        for (int i = 0; i < 54; i++) {
+        for (int i = 0; i < 6; i++) {
+        Agent_state = 2;
+
             if (animation_paused)
                 return NULL;
 
-            if (x > 1600) x = 0, Agent_x = 0;
             x += 40;
-            Copy(Map, x, y, 800, 450, Corridor);
-            //Deniz
-            if (c == 1) {
-                Copy(AgentX3, 53, 127, 62, 109, AgentStanding);  // Run 1
-            }
-            else if (c == 2) {
-                Copy(AgentX3, 112, 127, 67, 109, AgentStanding);  // Run 2
-            }
-            else if (c == 3) {
-                Copy(AgentX3, 175, 127, 54, 109, AgentStanding);  // Run 3
-            }
-            else if (c == 4) {
-                Copy(AgentX3, 230, 127, 56, 109, AgentStanding);  // Run 4
-            }
-            else {
-                Copy(AgentX3, 283, 127, 68, 109, AgentStanding);  // Run 5
-                c = 0;
-            }
-            c++;
-            //Eren
+            Agent_phase++;
+            Sleep(150);
             if (x == 401) {
-                Copy(Map, x, y, 800, 450, Corridor);
-                Copy(AgentX3, 345, 150, 60, 75, AgentStanding);   //Flip 1
-                Sleep(200);
-                PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-                DisplayImage(F1, Corridor);
-
-                Copy(Map, x, y, 800, 450, Corridor);
-                Copy(AgentX3, 405, 150, 60, 75, AgentStanding);   //Flip 2
-                PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-                DisplayImage(F1, Corridor);
-                Sleep(200);
-
-
-                Copy(Map, x, y, 800, 450, Corridor);
-                Copy(AgentX3, 465, 150, 60, 75, AgentStanding);   //Flip 3
-                PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-                DisplayImage(F1, Corridor);
-                Sleep(200);
-
-                Copy(Map, x, y, 800, 450, Corridor);
-                Copy(AgentX3, 525, 150, 60, 75, AgentStanding);   //Flip 4
-                DisplayImage(F1, Corridor);
-                PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-                DisplayImage(F1, Corridor);
-                Sleep(200);
+                Agent_phase = 0;
+                Agent_state = 3;
+                Sleep(150);
             }
-            c++;
-            Agent_x += 3;
-            PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-            DisplayImage(F1, Corridor);
-            Sleep(100);
+            if (Agent_state == 1) { 
+                break; 
+            }
         }
-
-        Copy(Map, x, y, 800, 450, Corridor);
-        Copy(AgentX3, 1, 127, 55, 109, AgentStanding); //Duran Adam
-        Agent_x += 3;
-        PasteNon0(AgentStanding, Agent_x, 250, Corridor);
-        DisplayImage(F1, Corridor);
-        animation_paused = true;
     }
     return 0;
 }
@@ -124,11 +153,11 @@ void ICGUI_main() {
 
     //BMP Pixel TEST
     //Copy(Map, x, y, 800, 450, Corridor);
-    //Copy(AgentX3, 345, 150, 60, 75, AgentStanding);   //Flip 1
-    //Copy(AgentX3, 405, 150, 60, 75, AgentStanding);   //Flip 2
-    //Copy(AgentX3, 465, 150, 60, 75, AgentStanding     //Flip 3
-    //Copy(AgentX3, 525, 150, 60, 75, AgentStanding     //Flip 4
-    //PasteNon0(AgentStanding, Agent_x, 250, Corridor);
+    //Copy(AgentX3, 345, 150, 60, 75, AgentCurrent);   //Flip 1
+    //Copy(AgentX3, 405, 150, 60, 75, AgentCurrent);   //Flip 2
+    //Copy(AgentX3, 465, 150, 60, 75, AgentCurrent     //Flip 3
+    //Copy(AgentX3, 525, 150, 60, 75, AgentCurrent     //Flip 4
+    //PasteNon0(AgentCurrent, Agent_x, Agent_y, Corridor);
     //DisplayImage(F1, Corridor);
     //Sleep(1000);
 
@@ -136,6 +165,9 @@ void ICGUI_main() {
     DisplayImage(F2, Agent);
 
     ICG_Button(544, 30, 160, 55, "Start/Stop Animation", StartStopAnimation);
+
+    // Yeni thread baþlat
+    _CreateThread(th, ScreenControllerThread);
 }
 
 
